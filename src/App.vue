91fe6269e9b926de2318dd407373030f7229dfc2
paulbarre <paul.yvon.barre@gmail.com>
2019-07-05 23:15:37 +0900

    Update README.md and sample

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,41 @@
 <template>
-    <div id="app">
-        <div><input placeholder="email" type="text" v-model="email"></div>
-        <div><input placeholder="password" type="password" v-model="password"></div>
-        <div><button @click="signin">Login with email</button></div>
-        <p style="color: red">{{ error }}</p>
-        <p>Am i logged: {{ $logged }}</p>
-        <div><button @click="signOut">Sign out</button></div>
-    </div>
+  <div id="app">
+    <div><input placeholder="email" type="text" v-model="email"></div>
+    <div><input placeholder="password" type="password" v-model="password"></div>
+    <div><button @click="handleError(signIn)">Login with email</button></div>
+    <p class="error">{{ error }}</p>
+    <p>Am i logged: {{ $logged }}</p>
+    <div><button @click="handleError(signOut)">Sign out</button></div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'app',
-    data () {
-        return {
-            email: '',
-            password: '',
-            error: ''
-        }
-    },
-    methods: {
-        async signin () {
-            try {
-                const response = await this.$auth.signInWithEmailAndPassword(this.email, this.password)
-                this.error = ''
-            } catch (error) {
-                this.error = error.message
-            }
-        },
-
-        async signOut () {
-            try {
-                const response = await this.$auth.signOut()
-                this.error = ''
-            } catch (error) {
-                this.error = error.message
-            }
-        }
+  name: 'app',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
     }
+  },
+  methods: {
+    async handleError (fn) {
+      try {
+        await fn()
+        this.error = ''
+      } catch (error) {
+        this.error = error.message
+      }
+    },
+
+    async signIn () {
+      await this.$auth.signInWithEmailAndPassword(this.email, this.password)
+    },
+
+    async signOut () {
+      await this.$auth.signOut()
+    }
+  }
 }
 </script>
